70c753bfba1c657a6f913f9cd06c3c5463b4aaf0
Jass <jass@MacBook-Air-de-Huncho-2.local>
2024-11-27 11:54:13 +0100

    add new line code

--- src/utils/movieList.ts
+++ src/utils/movieList.ts
@@ -1,5 +1,16 @@
 import movies from "../fixtures/movies.json";
 
-const utils = movies;
+const utils = movies.movies;
+
+movies.movies.reduce((acc, next) => {
+    return {
+        ...acc,
+        [next.year]: [...(acc[next.year] || []), next]
+    };
+}, {});
+
+export default utils;
+
+
 
 
